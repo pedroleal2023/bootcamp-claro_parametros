31c26bc9be8676d526ad9e903713e950ea6986fd
Pedro Miguel <rudaprod.oficial@gmail.com>
2024-08-21 11:20:59 -0300

    Tratando parametros com Controle de Fluxos.

--- src/application/Contador.java
+++ src/application/Contador.java
@@ -1,10 +1,13 @@
 package application;
 
 import exceptions.ParametrosInvalidosException;
+
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         System.out.print("Digite o primeiro parâmetro: ");
         int parametroUm = sc.nextInt();
